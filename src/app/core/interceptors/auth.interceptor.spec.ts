cbac91f6bea1b31c560e4758a3398ebc064dd4d1
Jose Valdivia <jvaldivm@emeal.nttdata.com>
2023-12-16 13:15:19 -0500

    feat: add auth interceptor

---
+++ src/app/core/interceptors/auth.interceptor.spec.ts
@@ -0,0 +1,10 @@
+import { TestBed } from '@angular/core/testing';
+import { AuthInterceptor } from './auth.interceptor';
+
+describe('AuthInterceptor', () => {
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [AuthInterceptor],
+    })
+  );
+});
