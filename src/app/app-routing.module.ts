6360d049dbb0fc2fd60f83806f2e17fd220a0aaa
Jose Valdivia <jvaldivm@emeal.nttdata.com>
2023-12-16 11:44:08 -0500

    feat: routing to financial product window

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './core/components/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'financial-product',
+      },
+      {
+        path: 'financial-product',
+        loadComponent: () =>
+          import(
+            './windows/financial-product/financial-product.component'
+          ).then((m) => m.FinancialProductComponent),
+      },
+    ],
+    runGuardsAndResolvers: 'always',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
