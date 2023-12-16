7450d6fc17c32b96c59f752d304921db2d173826
Jose Valdivia <jvaldivm@emeal.nttdata.com>
2023-12-16 11:26:28 -0500

    feat: inital confirmation modal

---
+++ src/app/shared/components/confirmation-modal/confirmation-modal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-confirmation-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './confirmation-modal.component.html',
+  styleUrls: ['./confirmation-modal.component.scss']
+})
+export class ConfirmationModalComponent {
+
+}
