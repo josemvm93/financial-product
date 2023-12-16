df5ff236241a9c148c497beeeb910e1653768856
Jose Valdivia <jvaldivm@emeal.nttdata.com>
2023-12-16 14:35:10 -0500

    feat(footer): add text

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
-
+  /**
+   * Current year
+   *
+   * @type {number}
+   */
+  currentYear = new Date().getFullYear();
 }
