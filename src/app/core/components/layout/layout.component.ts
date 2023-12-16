864936719869bd3c03ad7927fd383a22520522c2
Jose Valdivia <jvaldivm@emeal.nttdata.com>
2023-12-16 11:50:12 -0500

    feat: layout structure

--- src/app/core/components/layout/layout.component.ts
+++ src/app/core/components/layout/layout.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
+import { MainComponent } from '../main/main.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, FooterComponent, MainComponent],
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent {
-
-}
+export class LayoutComponent {}
--- src/app/core/components/main/main.component.ts
+++ src/app/core/components/main/main.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
