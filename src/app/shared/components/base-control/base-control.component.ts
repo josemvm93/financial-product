1b66d9a9a2a1cfd9dc6c7713fa4f2c60137a122b
Jose Valdivia <jvaldivm@emeal.nttdata.com>
2023-12-17 09:17:51 -0500

    feat: add base control component

---
+++ src/app/shared/components/base-control/base-control.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-base-control',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './base-control.component.html',
+  styleUrls: ['./base-control.component.scss']
+})
+export class BaseControlComponent {
+
+}
