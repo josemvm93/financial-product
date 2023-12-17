315f73c543d8fc3d494b4c6080727801c2999ffd
Jose Valdivia <jvaldivm@emeal.nttdata.com>
2023-12-17 15:49:44 -0500

    feat(layout): add loading

--- src/app/core/components/layout/layout.component.ts
+++ src/app/core/components/layout/layout.component.ts
@@ -1,14 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { LoadingService } from '@core/services/loading.service';
+import { Observable } from 'rxjs';
 import { FooterComponent } from '../footer/footer.component';
 import { HeaderComponent } from '../header/header.component';
+import { LoadingComponent } from '../loading/loading.component';
 import { MainComponent } from '../main/main.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent, MainComponent],
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    LoadingComponent,
+  ],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  constructor(private loadingService: LoadingService) {}
+
+  get loading$(): Observable<boolean> {
+    return this.loadingService.loading$;
+  }
+}
