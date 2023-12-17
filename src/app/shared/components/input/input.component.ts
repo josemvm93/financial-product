500b35ae5b7c17399705bd06271c78d8c6baaca0
Jose Valdivia <jvaldivm@emeal.nttdata.com>
2023-12-17 16:43:13 -0500

    refactor: input

--- src/app/shared/components/input/input.component.ts
+++ src/app/shared/components/input/input.component.ts
@@ -2,14 +2,6 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BaseControlDirective } from '@shared/directives/base-control.directive';
-import {
-  BehaviorSubject,
-  debounceTime,
-  distinctUntilChanged,
-  filter,
-  startWith,
-  takeUntil,
-} from 'rxjs';
 
 @Component({
   selector: 'app-input',
@@ -33,30 +25,6 @@ export class InputComponent {
    */
   @Input() label!: string;
   /**
-   * Min length to release value
-   *
-   * @type {number}
-   */
-  @Input() minLength = 1;
-  /**
-   * Delay
-   *
-   * @type {number}
-   */
-  @Input() delay = 300;
-  /**
-   * Input change to search
-   *
-   * @type {EventEmitter<string>}
-   */
-  @Output() inputChangeToSearch = new EventEmitter<string>();
-  /**
-   * Input change
-   *
-   * @type {EventEmitter<string>}
-   */
-  @Output() inputChange = new EventEmitter<string>();
-  /**
    * On blur
    *
    * @type {EventEmitter<string>}
@@ -68,41 +36,13 @@ export class InputComponent {
    * @type {*}
    */
   controlDirective = inject(BaseControlDirective);
-  /**
-   * Value Subject
-   *
-   * @private
-   * @type {BehaviorSubject<string>}
-   */
-  private value$ = new BehaviorSubject<string>('');
 
   /**
    * Creates an instance of InputTextComponent
    *
    * @constructor
    */
-  constructor() {
-    this.value$
-      .pipe(
-        startWith(''),
-        debounceTime(this.delay),
-        filter((v: string) => v?.length >= this.minLength),
-        distinctUntilChanged(),
-        takeUntil(this.controlDirective.destroy$)
-      )
-      .subscribe((v) => this.inputChangeToSearch.next(v));
-  }
-  /**
-   * On input change
-   *
-   * @param {string} value Value
-   */
-  onInputChange(event: Event): void {
-    const inputValue = (event.target as HTMLInputElement).value;
-    this.inputChange.next(inputValue);
-    this.value$.next(inputValue);
-    this.controlDirective.control.setValue(inputValue);
-  }
+  constructor() {}
   /**
    * On input blur
    */
