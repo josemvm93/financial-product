f184a825e62aed71198b6ad14d88f9b5eb8894e8
Jose Valdivia <jvaldivm@emeal.nttdata.com>
2023-12-17 19:20:39 -0500

    test: product service

--- src/app/core/services/financial-product.service.spec.ts
+++ src/app/core/services/financial-product.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FinancialProductService } from './financial-product.service';
 
 describe('FinancialProductService', () => {
   let service: FinancialProductService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [FinancialProductService],
+    });
     service = TestBed.inject(FinancialProductService);
   });
 
